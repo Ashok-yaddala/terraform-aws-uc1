56a9d638c497aada9cdecf9826d82e2086817901
Ashok-yaddala <Ashokyaddala4devops@gmail.com>
2025-07-10 11:55:58 +0530

    Update nginx-3.sh

--- nginx-3.sh
+++ nginx-3.sh
@@ -1,6 +1,1 @@
-#!/bin/bash
-sudo yum update -y
-sudo amazon-linux-extras install nginx1 -y
-sudo systemctl start nginx
-sudo systemctl enable nginx
-echo "Register! (Instance C)" | sudo tee /usr/share/nginx/html/register.html
+
