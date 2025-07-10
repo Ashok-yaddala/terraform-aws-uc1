da9a74fd45a1ac08a44cc5d3563082f177aae321
Ashok-yaddala <Ashokyaddala4devops@gmail.com>
2025-07-10 11:55:47 +0530

    Update nginx-2.sh

--- nginx-2.sh
+++ nginx-2.sh
@@ -1,6 +1,1 @@
-#!/bin/bash
-sudo yum update -y
-sudo amazon-linux-extras install nginx1 -y
-sudo systemctl start nginx
-sudo systemctl enable nginx
-echo "Images! (Instance B)" | sudo tee /usr/share/nginx/html/image.html
+
